62d650a1830815b4bca1ed633a95cdd5357b9c80
Dawit Michael <32758991+dawit-michael@users.noreply.github.com>
2021-09-21 07:14:30 +0300

    styling

--- src/components/my-todos.tsx
+++ src/components/my-todos.tsx
@@ -5,20 +5,28 @@ import { RootState } from "../app/store";
 interface Props {}
 const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 function MyTodos({}: Props): ReactElement {
+  // use dispatcher to manage state and actions
   const dispatch = useDispatch();
+  //   selectes all data in store
   const todos = useTypedSelector(selectTodos);
   return (
-    <div>
+    //   container
+    <div className="border  mt-14 w-1/4 rounded-lg px-6 py-6 ">
+      {/*  list of tasks */}
       <ul>
         {todos.map((todo) => {
           return (
-            <li>
+            <li
+              key={todo.id}
+              className="bg-gray-50 rounded-lg text-xl capitalize flex justify-between items-center py-3 px-8 mb-6 hover:shadow-xl"
+            >
+              <span> {todo.title}</span>
               <input
+                key={todo.id}
                 type="checkbox"
                 checked={todo.completed}
                 onChange={() => dispatch(toggleTodo(todo.id))}
               />
-              {todo.title}
             </li>
           );
         })}
